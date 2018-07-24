12f81d757b2b3bd728fd22d6c841232edb8b94c5
leefine <lihuai.chen@163.com>
2018-07-24 10:41:22 +0800

    支持用户管理、删除权限控制

---
+++ user.index.php
@@ -0,0 +1,26 @@
+<?php $auth_users = array (
+  'admin' => 
+  array (
+    'pwd' => 'dbf266a895687fae9cea542d8adfcbf9',
+    'rw' => 'rwd',
+    'um' => 'y',
+    'ro' => '/',
+    'cb' => '',
+  ),
+  'wangjia' => 
+  array (
+    'pwd' => '6f8ffc7802f710bf1fafe2f9626a9fd7',
+    'rw' => 'rw',
+    'um' => 'n',
+    'ro' => '/',
+    'cb' => 'admin',
+  ),
+  '233@sdfs.comsdf' => 
+  array (
+    'pwd' => 'ecf6d9e4b0cdf38512b1e662f2c4c5b6',
+    'rw' => 'r',
+    'um' => 'y',
+    'ro' => '/',
+    'cb' => 'admin',
+  ),
+);
